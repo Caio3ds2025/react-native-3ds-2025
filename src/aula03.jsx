eab0db78f2d034b4389eba00ac24c45b848cbbdd
Caio3ds2025 <caio.peron@escola.pr.gov.br>
2025-05-20 07:50:59 -0300

    Add files via upload

--- src/aula03.jsx
+++ src/aula03.jsx
@@ -2,11 +2,28 @@ import React, {Component} from "react";
 import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 
 class Aula03 extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundColor: 'blueviolet',
+        }
+    }
+    changeColor = () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        this.setState({backgroundColor: randomColor});
+    }
+    
     render() {
+
+        
+
         return(
             <View style={ styles.container }>
-                <TouchableOpacity style={styles.botao}>
-                    <Text>Botão</Text>
+                <TouchableOpacity style={[styles.botao, {backgroundColor: this.backgroundColor}]}
+                onPress={ this.changeColor}
+                >
+                    <Text style={styles.textbotao}>Toque para trocar a cor do fundo</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -25,5 +42,12 @@ const styles = StyleSheet.create({
 
     botao: {
         backgroundColor: 'blue',
+        padding: 15,
+        borderRadius: 5,
+    },
+
+    textbotao: {
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
